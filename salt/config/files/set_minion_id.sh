4f6629a881d03cb75a28bfe98812b0a57566919c
gervasek <gervasek@users.noreply.github.com>
2019-07-15 11:34:18 +0200

    Fix set_minion_id.sh (#129)

--- salt/config/files/set_minion_id.sh
+++ salt/config/files/set_minion_id.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+sleep 60
+
+export HOME="/home/ec2-user"
+
 # Set minion_id
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 INSTANCE_NAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
-echo "$INSTANCE_NAME-$AWS_INSTANCE_ID" | sudo tee /etc/salt/minion_id
+if [ "$INSTANCE_NAME" != "" ]; then
+  echo "$INSTANCE_NAME-$AWS_INSTANCE_ID" | sudo tee /etc/salt/minion_id
+fi
