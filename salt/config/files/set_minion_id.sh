512ce67e79ddadafbef9fd379acfd928454718c1
Gervais Sébastien <gervasek@users.noreply.github.com>
2019-10-16 14:22:36 +0200

    remove output on set_minion_id (#132)

--- salt/config/files/set_minion_id.sh
+++ salt/config/files/set_minion_id.sh
@@ -10,5 +10,5 @@ aws sts assume-role --role-arn "arn:aws:iam::469132580751:role/Cloud9Role" --rol
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 INSTANCE_NAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --profile Cloud9Role --output text | cut -f5)
 if [ "$INSTANCE_NAME" != "" ]; then
-  echo "$INSTANCE_NAME-$AWS_INSTANCE_ID" | sudo tee /etc/salt/minion_id
+  echo "$INSTANCE_NAME-$AWS_INSTANCE_ID" | sudo tee /etc/salt/minion_id &>/dev/null
 fi
