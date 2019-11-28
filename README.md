8e9ae921e49cd692dd87cdabab801904623af238
Sébastien GERVAIS <gervais.seb@gmail.com>
2019-11-28 16:15:47 +0000

    Add cloud9NotStoppedAlert

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Lambda scripts to manage cloud9 instances
 ## Instructions:
 
 
-Python scripts 2.7 without dependances.
+Python scripts 3.8 with request dependances.
 
-lambdaFunction.py: enlarge EC2 volume of Cloud9 environement. Skip resize if 'noEBSresize' present on C9 description.
+lambdaFunction.py: Send slack notification for EC2 cloud9 instances started more than 8 hours.
 
 Script triggered by [CloudWatch Event](cloudWatch.Event)
 
--- lambda_function.py
+++ lambda_function.py
@@ -1,102 +1,89 @@
 import boto3
-ec2R = boto3.resource('ec2')
-ec2 = boto3.client('ec2')
-c9 = boto3.client('cloud9')
-
-from botocore.exceptions import ClientError
+import datetime
+import os
+import requests
 
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-debugMode = False
+ec2_cli = boto3.client('ec2')
 
-DRYRUN = False
-if (debugMode):
-  logger.setLevel(logging.DEBUG)
-  DRYRUN = True
+#List european zone
+regions = []
+regions_cli = ec2_cli.describe_regions()["Regions"]
 
-EBS_SIZE = 20
-EBS_SIZE_DG = {'EAC': 40}
+for region in regions_cli:
+    regions.append(region["RegionName"])
 
 def lambda_handler(event, context):
-  if (event['detail']['event'] == "createVolume" ):
-    if (event['detail']['result'] != "available"):
-      logger.error("Volume not ready")
-    else:
-      # Get volume from event
-      ressources = event['resources'][0]
-      arn,volumeID = ressources.split(":volume/")
-      volume = ec2R.Volume(volumeID)
-      logger.info("Volume {0} start resize".format(volumeID) )
-      
-      # Get EC2 attached instance ID 
-      if  len(volume.attachments) == 0:
-        logger.info("Volume {0} skipped, not attached to an EC2 instance".format(volumeID))
-        return False
-      else:
-        EC2Id=volume.attachments[0]['InstanceId']
-        ec2instance = ec2R.Instance(EC2Id)
-      
-      # Ensure it's volume of cloud9 environment
-      isC9=False
-      for tags in ec2instance.tags:
-        if tags["Key"] == 'aws:cloud9:environment':
-          isC9 = True
-          # if 'noebsresize' is present on description, don't resize volume
-          response = c9.describe_environments(environmentIds=[tags["Value"]])
-          c9env  = response['environments'][0]
-          if "noebsresize" in c9env['description'].lower():
-            logger.info("Volume {0} skipped: 'noebsresize' found on C9 description".format(volumeID))
-            return False
-          break;
-          
-      if isC9:
-        # Call tagEC2InstancesAndVolumes lambda to have DG tag available
-        lambdaC = boto3.client('lambda')
-        response = lambdaC.invoke(
-          FunctionName='tagEC2InstancesAndVolumes',
-          InvocationType='RequestResponse',
-          Payload='{"instanceId" : "'+ EC2Id +'"}'
-        )
-        logger.info("tagEC2InstancesAndVolumes called for EC2Id '{0}'. Response: {1}".format(EC2Id, response) )
-        # Reload volume after tagging
-        volume = ec2R.Volume(volumeID)
+    
+    count = 0
+    message = ""
+    
+    #Set date - 8 hours
+    start_date = datetime.datetime.today() - datetime.timedelta(hours=8)
+    print("Start " + str(start_date))
+    
+    for region in regions :
+        
+        ec2_res = boto3.resource('ec2', region_name = region)
+        #####Get EC2
+        #Filter get running ec2
+        filters = [
+            {
+                'Name': 'instance-state-name', 
+                'Values': ['running']
+            }
+        ]
+        
+        #Get instances by filters
+        instances = ec2_res.instances.filter(Filters=filters)
+        ec2_list = []
+        
+        regionReport = ""
+        #Parse all instances which are running or stopped
+        for instance in instances :
+            if(instance.launch_time.replace(tzinfo=None) < start_date):
+                cloud9 = False
+                name = ""
+                for j in range(len(instance.tags)):
+                    if instance.tags[j]["Key"] == "Name":
+                        name = instance.tags[j]["Value"]
+                    if instance.tags[j]["Key"] == "aws:cloud9:environment":
+                        cloud9 = True
+                if cloud9 :
+                    count = count + 1
+                    instance_description  = " --> " + name + " (" + instance.id + " / " + str(instance.launch_time) + ") : " + instance.instance_type + "\n"
+                    logger.info(instance_description)
+                    regionReport = regionReport + instance_description
+        if not regionReport == "":
+            message = "### " + region + " \n" + regionReport + message
+    
+    if not message == "" :
+        message = "These " + str(count) + " Cloud9 have been started since more than 8 hours :\n" + message
+        message = "######################\n" + message
+    
+        print(message)
+        logger.debug ("Message: {0}".format(message))
+        logger.info ("Send slack message...")
+        result = sendSlackMessage(message)
+        #result = 200
 
-        # Get DG tag of volume
-        for tags in volume.tags:
-          if tags["Key"] == 'DG':
-            tagDG = tags["Value"]
-
-        # Get new size by DG
-        newSize = EBS_SIZE
-        if tagDG in EBS_SIZE_DG:
-          newSize=EBS_SIZE_DG[tagDG]
-          logger.info("Volume {0} have DG '{1}', resize value: {2}Go".format(volumeID, tagDG, newSize) )
-
-        curSize = volume.size
-        if curSize >= newSize:
-        # Size Ok, skip
-          logger.info("Volume {0} skipped: size OK: {1}Go".format(volumeID, curSize) )
+        if result == 200:
+            logger.info ("Slack notification sent.")
         else:
-        # Edit Volume
-          response = False
-          try:
-            response = ec2.modify_volume(
-                DryRun=DRYRUN,
-                VolumeId=volumeID,
-                Size=newSize
-            )
-          except ClientError as e:
-            logger.debug(e)
-            #An error occurred (VolumeModificationRateExceeded) when calling the ModifyVolume operation: You've reached the maximum modification rate per volume limit. Wait at least 6 hours between modifications per EBS volume.
-            logger.error("Volume {0} failed: ClientError Exception: {1}".format(volumeID, e))
-            return False
-          if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            logger.info("Volume {0} updated: size changed from {1}Go to {2}Go".format(volumeID, curSize, newSize))
-            return True
-          else:
-            logger.error("Volume {0} failed: Target {1}Go, Current Size: {2}. Responde: {3}".format(volumeID, newSize,curSize, response))
-      else:
-        logger.info("Volume {0} skipped: not cloud9 environment".format(volumeID))
-  return False
+            logger.error ("Slack notification error, return code: %d" % (result))
+    
+def sendSlackMessage (message) :
+    SLACK_URL=os.environ['webhook_URL']
+    SLACK_CHANNEL=os.environ['channel']
+    SLACK_USERNAME=os.environ['username']
+    SLACK_ICON=os.environ['icon_emoji']
+    try:
+        response = { "text" : message, "channel": SLACK_CHANNEL, "username": SLACK_USERNAME, "icon_emoji": SLACK_ICON}
+        slack_response = requests.post(SLACK_URL,json=response, headers={'Content-Type': 'application/json'})
+        return slack_response.status_code
+    except Exception as inst:
+        logger.error ("Slack Callout Failed.Message: '%s'" % (inst))
+        return 0
