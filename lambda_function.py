5df0105e0c4eb612d03f8a4a28cfb5185c3aa4f9
Sébastien GERVAIS <gervais.seb@gmail.com>
2019-11-28 11:12:43 +0000

    Some adjustments

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,9 @@
-import json
 import boto3
 import datetime
 import os
-import configparser
 import requests
 
-START_DATE = 60
+START_DATE = 180
 
 import logging
 logger = logging.getLogger(__name__)
@@ -21,24 +19,26 @@ def lambda_handler(event, context):
 
   start_date = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=START_DATE)
   logger.info ("Check user not connected since " + start_date.strftime("%A %d. %B %Y"))
-  message = ["Users with no recent activity on account digit-d1-nexteuropa-dev of AWS console"]
+  message = []
   paginator = iam.get_paginator('list_users')
   for page in paginator.paginate():
     for user in page['Users']:
-      if "PasswordLastUsed" in user:
-        if user['PasswordLastUsed'] < start_date:
-          mess="{} User {} not connected to AWS console since {}".format(chr(8226), user['UserName'], user['PasswordLastUsed'].strftime("%Y-%m-%d"))
+      if user['UserName'] != "drone-lambda-cd":
+        if "PasswordLastUsed" in user:
+          if user['PasswordLastUsed'] < start_date:
+            mess="{} User {} not connected to AWS console since {}".format(chr(8226), user['UserName'], user['PasswordLastUsed'].strftime("%Y-%m-%d"))
+            message.append(mess)
+            logger.debug (mess)
+            i=i+1
+        else:
+          mess="{} User {} never connected to AWS console".format(chr(8226), user['UserName'])
           message.append(mess)
           logger.debug (mess)
           i=i+1
-      else:
-        mess="{} User {} never connected to AWS console".format(chr(8226), user['UserName'])
-        message.append(mess)
-        logger.debug (mess)
-        i=i+1
 
   logger.info ("{} users found.".format(i))
   if i != 0:
+    message = ["There are {} users with no recent activity on account digit-d1-nexteuropa-dev of AWS console:".format(i), *message]
     logger.info ("Send slack message...")
 
     result = sendSlackMessage('\n'.join(message))
@@ -49,17 +49,11 @@ def lambda_handler(event, context):
         logger.error ("Slack notification error, return code: %d" % (result))
 
 def sendSlackMessage (message) :
-    CONFIGFILE=  os.path.dirname(__file__) + "/lambda_config.ini"
-    if not os.path.isfile(CONFIGFILE):
-        logger.error("Config file not fount at '{0}'".format(CONFIGFILE))
-        return False
-    config = configparser.ConfigParser()
-    config.read(CONFIGFILE)
     try:
-        SLACK_URL=config.get('slack', 'webhook_URL')
-        SLACK_CHANNEL=config.get('slack', 'channel')
-        SLACK_USERNAME=config.get('slack', 'username')
-        SLACK_ICON = config.get('slack', 'icon_emoji')
+        SLACK_URL=os.environ['webhook_URL']
+        SLACK_CHANNEL=os.environ['channel']
+        SLACK_USERNAME=os.environ['username']
+        SLACK_ICON=os.environ['icon_emoji']
         response = { "text" : message, "channel": SLACK_CHANNEL, "username": SLACK_USERNAME, "icon_emoji": SLACK_ICON}
         slack_response = requests.post(SLACK_URL,json=response, headers={'Content-Type': 'application/json'})
         return slack_response.status_code
